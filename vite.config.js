2829429c31442b209090adc5a170548cbd22163f
Karan Ankade <karanankade12@gmail.com>
2026-08-19 21:32:40 +0530

    Optimize Vite build: configure chunk splitting and chunkSizeWarningLimit

--- vite.config.js
+++ vite.config.js
@@ -15,4 +15,16 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/three') || id.includes('node_modules/@react-three')) {
+            return 'three-bundle';
+          }
+        }
+      }
+    }
+  }
 })
